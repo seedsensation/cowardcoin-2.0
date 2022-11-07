b7a1a200b41eea63ca2e0b7f29da77fdc8efed7c
seeds <seedsensation@icloud.com>
2022-11-07 21:19:30 +0000

    Added Style Points™ to the Help command

--- commands_folder/help_file.py
+++ commands_folder/help_file.py
@@ -3,7 +3,15 @@ from global_context import *
 commandsdefinition = {
     "get":"<a:gold:1038495846074941440> **GET**\nThis command collects any available coins.\nIf there are no coins available, the bot might be a little annoyed at being asked.",
     "leaderboard":"<a:gold:1038495846074941440> **LEADERBOARD**\nThis command shows you the users with the top 5 highest number of coins. Compare yourselves and fight with your friends :)",
-    "help":"I don't think you really need help with that one..."
+    "help":"I don't think you really need help with that one...",
+    "style":"""<a:gold:1038495846074941440> **Style Points™**
+    Style points are given out when you do cool Coin Tricks! You complete a coin trick by using the `!coin give` command to give yourself any amount of coins.
+    This will award you a random amount of Style Points™ - ranging between 1 and 100.
+    If you gain 90 or more Style Points™ in a single trick, the amount of coins you put into it will be doubled!
+    However, if you gain 25 or less Style Points™ in a single trick, you will lose 1/3 of the coins you put in (rounded up).
+    Also, you can only pull off one Coin Trick every 24 hours.
+    If you want to see how many Style Points™ you have, you can simply do `!coin count`! This will display your Style Points™ along with your coins if you have any.
+    If you want to compete, you can use `!coin leaderboard style` - this will display an alternate leaderboard for Style Points™ instead of coins."""
 }
 
 async def help_coin(ctx,arg):
