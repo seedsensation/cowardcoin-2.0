db5b77b746c9a8a4bc0507554282ab568d6b57b5
seeds <seedsensation@icloud.com>
2022-11-07 20:57:30 +0000

    StylePoints™ only show up in Coin Count if you've earned any.

--- commands_folder/count_file.py
+++ commands_folder/count_file.py
@@ -2,4 +2,7 @@ from global_context import *
 
 async def count_coin(ctx):
     if ctx.author.id in context[2]:
-        await ctx.send("<a:gold:1038495846074941440> You have "+str(context[2][ctx.author.id][0])+" coins in your account.\nYou also have "+str(context[2][ctx.author.id][1])+" Style Points™!")
+        output = ("<a:gold:1038495846074941440> You have "+str(context[2][ctx.author.id][0])+" coins in your account.")
+        if context[2][ctx.author.id][1] > 0:
+            output += "\nYou also have "+str(context[2][ctx.author.id][1])+" Style Points™!"
+        ctx.send(output)
