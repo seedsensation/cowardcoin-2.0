e1343eb7cffb6f163305ff916c6395e160ccff62
seeds <seedsensation@icloud.com>
2022-11-07 17:26:16 +0000

    Fixed error creating coin

--- commands_folder/create_file.py
+++ commands_folder/create_file.py
@@ -12,7 +12,7 @@ async def create_command(ctx):
     await create_coin(ctx)
 
 async def create_coin(ctx):
-    with open(Path("files\\images\\gold.gif","rb")) as f:
+    with open(Path("files/images/gold.gif","rb")) as f:
         picture = discord.File(f)
     context[1] = await ctx.send("A new coin has appeared!\nType !coin get to claim it!",file=picture)
     print("Created Coin")
