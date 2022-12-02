1fdfc18326226bd28fe3f498650e2e4d2ae7f04c
seedsensation <84081520+seedsensation@users.noreply.github.com>
2022-12-02 10:48:10 +0000

    Update global_context.py
    Added coin boundaries to global context

--- global_context.py
+++ global_context.py
@@ -11,8 +11,10 @@ context = [
     30,         # 4 - minimum number of seconds before next coin  (default: 30)
     3600,       # 5 - maximum number of seconds before next coin (default: 3600)
     True,       # 6 - does a coin need to be created?
-    120,         # 7 - time until coin expires
+    120,        # 7 - time until coin expires
     21600,      # 8 - minimum time between tricks
+    25,         # 9 - upper bound for losing coins
+    90,         # 10 - lower bound for doubling coins
 ]
 
 insults = [
