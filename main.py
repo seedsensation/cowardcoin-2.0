fbfc7860e140af5ad47f5bf7120913b38d79db5c
seedsensation <84081520+seedsensation@users.noreply.github.com>
2022-12-21 09:25:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -54,9 +54,9 @@ commandslist = {"echo":[1,echo_command],"get":[0,get_command],"leaderboard":[1,l
 
 @bot.command()
 async def coin(ctx,*args):
-    if args[0] in commandslist.keys():
+    if args[0] in commandslist:
         print(args[0])
-        if commandslist[args[0]] == 1:
+        if commandslist[args[0]][0] == 1:
             await commandslist[args[0]][1](ctx,args)
         else:
             await commandslist[args[0]][1](ctx)
