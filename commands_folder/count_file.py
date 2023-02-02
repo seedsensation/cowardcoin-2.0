7c50e4bcabb74df014751718a14b7c3648cfab86
seeds <seedsensation@icloud.com>
2023-02-02 22:30:19 +0000

    Upgraded Management

--- commands_folder/count_file.py
+++ commands_folder/count_file.py
@@ -2,7 +2,11 @@ from global_context import *
 
 async def count_coin(ctx):
     if ctx.author.id in context[2]:
-        output = ("<a:gold:1038495846074941440> You have "+str(context[2][ctx.author.id][0])+" coins in your account.")
+        if ctx.author.id == 1070754908845912154:
+            count = "∞"
+        else:
+            count = context[2][ctx.author.id][0]
+        output = ("<a:gold:1038495846074941440> You have "+str(count)+" coins in your account.")
         if context[2][ctx.author.id][1] > 0:
             output += "\nYou also have "+str(context[2][ctx.author.id][1])+" Style Points™!"
         if context[11][0] == ctx.author.id and context[11][1] > 1:
